98411cd28e8b3897d1d90a7de9907ca502f00612
unknown <inriveronika17@gmail.com>
2023-04-17 11:52:56 +0700

    feat body page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <div>hai</div>
+    </Layout>
+  );
+};
+
+export default Home;
